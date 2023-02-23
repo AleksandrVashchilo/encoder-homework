fec177764d011a6b811af54833ddb31ab52ebc4a
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-23 22:35:05 +0100

    First commit

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Encoder {
+import java.util.Stack;
+
+public class Encoder {
+
+    public void encoder(String line) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < line.length(); i++) {
+            char symbol = line.charAt(i);
+            if (stack.size() < 5) {
+                stack.push(symbol);
+            }
+            if (stack.size() == 5 || i == line.length() - 1){
+               while (!stack.isEmpty()) {
+                   System.out.println(stack.pop());
+               }
+            }
+        }
+    }
 }
