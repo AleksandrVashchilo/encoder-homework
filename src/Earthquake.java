b617ba4836105ee713f68f5473875280dd4375e6
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-24 16:59:36 +0100

    Second commit

---
+++ src/Earthquake.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Earthquake {
+
+    private static final int MAGIC_NUMBER = -97;
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        Scanner scanner = new Scanner(System.in);
+        int quake;
+
+        do {
+            quake = scanner.nextInt();
+            if (quake >= 3) {
+                queue.add(quake);
+            }
+        } while (quake != MAGIC_NUMBER);
+
+        if (queue.size() >= 5) {
+            System.out.println("Все плохо");
+            while (!queue.isEmpty()) {
+                System.out.println(queue.poll());
+            }
+        }
+    }
+}
